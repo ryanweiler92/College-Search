81cec9520161317b4b1fd58f502a93bd97bf0dde
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-31 12:02:53 -0500

    search functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,22 @@
-//api key apikey=8MDswbbw2Ak8pYG4Mol1zG39DJq9HTWt
-
-//kansas city chiefs api search https://app.ticketmaster.com/discovery/v2/attractions.json?id=K8vZ9171oMf&apikey=8MDswbbw2Ak8pYG4Mol1zG39DJq9HTWt
-
 
 var searchInput = document.getElementById("search");
 var searchBttn = document.getElementById("search-btn")
+var resultTable = document.getElementById("results-table")
 
 var getSearchValue = function (e){
     e.preventDefault();
 
     var search = searchInput.value;
     console.log(search);
+    //send zipcode to college search function
+    collegeZipSearch(search);
 }
 
 
 
-var ticketMasterApiCall = function () {
+var collegeZipSearch = function (zip) {
 
-    var apiUrl = "http://api.data.gov/ed/collegescorecard/v1/schools.json?zip=06085&distance=25mi&api_key=mzvkvI7tUG41Q8n90FJQmtCL7NdDczS5dhJW6pKO"
+    var apiUrl = "http://api.data.gov/ed/collegescorecard/v1/schools.json?zip="+ zip +"&distance=20mi&sort=latest.student.size:desc&api_key=mzvkvI7tUG41Q8n90FJQmtCL7NdDczS5dhJW6pKO"
 
     fetch(apiUrl).then(function(response){
         if (response.ok) {
@@ -27,13 +26,19 @@ var ticketMasterApiCall = function () {
 
             })
         } else {
-            console.log("Error: Artist not found")
+            console.log("Error: No schools found")
         }
     })
-
-
+    .catch(function(error) {
+        console.log("Unable to connect to API")
+    })
 }
 
-ticketMasterApiCall();
 
 searchBttn.addEventListener("click", getSearchValue)
+
+
+
+// http://api.data.gov/ed/collegescorecard/v1/schools.json?sort=latest.student.size&zip=06085&distance=20mi&api_key=mzvkvI7tUG41Q8n90FJQmtCL7NdDczS5dhJW6pKO
+
+//http://api.data.gov/ed/collegescorecard/v1/schools.json?zip=06085&distance=20mi&sort=latest.student.size&api_key=mzvkvI7tUG41Q8n90FJQmtCL7NdDczS5dhJW6pKO
