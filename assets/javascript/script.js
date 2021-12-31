25ecb708738a46e37814b44531d3fcb2a1944f71
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-31 13:15:13 -0500

    display results

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,8 @@
 
 var searchInput = document.getElementById("search");
-var searchBttn = document.getElementById("search-btn")
-var resultTable = document.getElementById("results-table")
+var searchBttn = document.getElementById("search-btn");
+var resultTable = document.getElementById("results-table");
+var tableBody = document.getElementById("table-body");
 
 var getSearchValue = function (e){
     e.preventDefault();
@@ -21,9 +22,8 @@ var collegeZipSearch = function (zip) {
     fetch(apiUrl).then(function(response){
         if (response.ok) {
             response.json().then(function(data){
-                console.log(data)
-
-
+                console.log(data);
+                displayResults(data);
             })
         } else {
             console.log("Error: No schools found")
@@ -33,6 +33,51 @@ var collegeZipSearch = function (zip) {
         console.log("Unable to connect to API")
     })
 }
+
+var displayResults = function (data){
+
+    for (i = 0; i <= data.results.length; i++){
+        var tableRow = document.createElement('tr');
+        tableBody.appendChild(tableRow);
+
+        var schoolName = document.createElement('td');
+        schoolName.textContent = data.results[i].latest.school.name;
+        tableRow.appendChild(schoolName);
+
+        var cityName = document.createElement("td");
+        cityName.textContent = data.results[i].latest.school.city;
+        tableRow.appendChild(cityName)
+
+        var schoolSize = document.createElement("td");
+        var schoolSizeNum = data.results[i].latest.student.size;
+        var schoolSizeComma = schoolSizeNum.toLocaleString("en-US")
+        schoolSize.textContent = schoolSizeComma
+        tableRow.appendChild(schoolSize);
+
+        var tuitionIn = document.createElement("td");
+        if (data.results[i].latest.cost.tuition.in_state == null){
+            tuitionIn.textContent = "no data"
+        } else {
+            var tuitionInNum = data.results[i].latest.cost.tuition.in_state;
+            var tuitionInComma = tuitionInNum.toLocaleString("en-US");
+            tuitionIn.textContent = "$"+ tuitionInComma;
+        }
+        tableRow.appendChild(tuitionIn)
+
+        var tuitionOut = document.createElement("td");
+        if (data.results[i].latest.cost.tuition.out_of_state == null){
+            tuitionOut.textContent = "no data"
+        } else{
+            var tuitionOutNum = data.results[i].latest.cost.tuition.out_of_state;
+            var tuitionOutComma = tuitionOutNum.toLocaleString("en-US");
+            tuitionOut.textContent = "$"+ tuitionOutComma
+        }
+        tableRow.appendChild(tuitionOut)
+
+
+    }
+}
+
 
 
 searchBttn.addEventListener("click", getSearchValue)
