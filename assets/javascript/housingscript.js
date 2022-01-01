d2f07d768e0ca702cbc8af004faefe7b0feb4769
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-31 22:30:39 -0500

    display jobs

--- assets/javascript/housingscript.js
+++ assets/javascript/housingscript.js
@@ -1,5 +1,7 @@
 
 
+var zipHeader = document.getElementById("zip")
+
 
 var getJobs = function(zip) {
     var apiUrl = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=03ab0e99&app_key=e527ac2e4dacd7eeb55525db9b15cac4&results_per_page=20&where=" + zip
@@ -8,6 +10,7 @@ var getJobs = function(zip) {
         if (response.ok) {
             response.json().then(function(data){
                 console.log(data);
+                displayJobs(zip, data);
             })
         } else {
             console.log("Error")
@@ -16,6 +19,49 @@ var getJobs = function(zip) {
     .catch(function(error) {
         console.log("Unable to connect to API")
     })
+
+
+}
+
+var displayJobs = function(zip, data) {
+
+zipHeader.textContent = zip;
+
+var tbody = document.getElementById("job-table-body")
+
+for (i = 0; i <= data.results.length; i++) {
+
+    console.log(data.results.length)
+
+    var tableRow = document.createElement("tr");
+    tbody.appendChild(tableRow);
+
+    var jobTitle = document.createElement("td");
+    jobTitle.textContent = data.results[i].title;
+    tableRow.appendChild(jobTitle);
+
+    var area = document.createElement("td");
+    area.textContent = data.results[i].location.display_name
+    tableRow.appendChild(area);
+
+    var company = document.createElement("td");
+    company.textContent = data.results[i].company.display_name;
+    tableRow.appendChild(company);
+
+    var linkSpan = document.createElement("a")
+    linkSpan.setAttribute("href", data.results[i].redirect_url)
+    tableRow.appendChild(linkSpan)
+
+    var jobLink = document.createElement("i");
+    jobLink.setAttribute("class", "material-icons")
+    jobLink.textContent = "search"
+    linkSpan.appendChild(jobLink)
+
+
+
+
+
+}
 
 
 }
